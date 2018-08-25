63abbece612ace7417962d96ded08c3046146a8c
Lindell Carter <lindellcarternyc@gmail.com>
2018-08-25 13:37:16 -0400

    remove dummy route

--- server.js
+++ server.js
@@ -25,12 +25,6 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
