39f4e8f6d392d6cd31d7a5398ed88bbd545e1aad
Lindell Carter <lindellcarternyc@gmail.com>
2018-08-26 12:40:58 -0400

    update post route

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+const utils = require('./utils')
+
 mongoose.Promise = Promise
 
 const bodyParser = require('body-parser')
@@ -19,6 +21,7 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGOLAB_URI);
+const db = mongoose.connection
 
 const URLSchema = new mongoose.Schema({
   url: { type: String, required: true }
@@ -34,22 +37,32 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(_, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/shorturl/new', (req, res) => {
-  const { body: url  } = req
-  console.log(url.url)
-
-  dns.lookup(url.url, (err, address) => {
-    if ( err ) {
+app.post('/api/shorturl/new', async (req, res) => {
+  const { body: { url } } = req
+  
+  console.log('got post request', url)
+  // Try to validate url
+  console.log('try to validate url')
+  utils.validateUrl(url)
+    .then( async () => {
+      // look for record with same url
+      const foundRecord = await URLModel.findOne({url})
+      if ( foundRecord !== null ) {
+        return res.json({original_url: url, short_url: foundRecord._id })
+      } else {
+        const newRecord = new URLModel({url})
+        await newRecord.save()
+        return res.json({original_url: url, short_url: newRecord._id})
+      }
+    })
+    .catch(err => {
       console.log(err)
-       return res.json({error: "Invalid url"})
-    }
-    console.log(address)
-    res.end()
-  })
+      res.send(err)
+    })
 })
 
 
